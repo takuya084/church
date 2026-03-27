0c5136c2041c8cdce3b43ff441c749b521ecfa1d
takuya084 <takuya084@gmail.com>
2026-03-27 14:00:23 +0900

    名前変更

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -9,7 +9,7 @@
 <link rel="icon" href="{{ asset('sda_favicon.ico') }}">
 <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('sda_favicon-32x32.png') }}">
 <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('sda_favicon-16x16.png') }}">
-<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('sda_apple-touch-icon.png') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
 
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 @fluxAppearance
